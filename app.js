1dc0737f0b491d3b68150653eb2dea07fcb8ff0f
AllanGame <61366528+AllanGame@users.noreply.github.com>
2020-12-18 15:58:03 -0500

    Bug fixes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const text_to_translate = document.getElementById('texttotranslate')
 function translateText() {  
     if(text_to_translate.value === "") {
+        window.navigator.vibrate(600)
         alert('Hey you haven\'t added anything to translate')
-        window.navigator.vibrate(2000)
     }
 }
