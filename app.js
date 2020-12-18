5a78cd79f9ac697f07abba706ba7c5895e5bfb77
AllanGame <61366528+AllanGame@users.noreply.github.com>
2020-12-18 15:14:41 -0500

    Added responsive desing

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-function translateText() {
-    document.getElementById('translatedtext').value = document.getElementById('normaltext').value
+const text_to_translate = document.getElementById('texttotranslate')
+function translateText() {  
+    if(text_to_translate.value === "") {
+        alert('Hey you haven\'t added anything to translate')
+        window.navigator.vibrate(2000)
+    }
 }
